fac7a78f8287f1b219fd4f3491437e4acd6b16d4
Luke Brezovec <brezovec@stanford.edu>
2022-04-19 17:06:18 -0700

    added translation

--- optomotor_suite_with_translation.py
+++ optomotor_suite_with_translation.py
@@ -1,6 +1,7 @@
 import socket
 from flystim.stim_server import launch_stim_server
 from flyrpc.transceiver import MySocketClient
+import flyrpc.multicall
 from flystim.screen import Screen
 from time import sleep
 from visprotocol.device import niusb
@@ -11,17 +12,17 @@ import time
 import os
 from random import sample
 
-use_bruker_server = False
+use_bruker_server = True
 
 if use_bruker_server:
-	host = '171.65.17.246'
-	port = 60629
-	manager = MySocketClient(host=host, port=port)
-	niusb_device = niusb.NIUSB6210(dev='Dev5', trigger_channel='ctr0')
+    host = '171.65.17.246'
+    port = 60629
+    manager = MySocketClient(host=host, port=port)
+    niusb_device = niusb.NIUSB6210(dev='Dev5', trigger_channel='ctr0')
 else:
-	aux_screen = Screen(id=0, fullscreen=False, vsync=True, square_size=(0.25, 0.25))
-	manager = launch_stim_server(aux_screen)
-	niusb_device = None
+    aux_screen = Screen(id=0, fullscreen=False, vsync=True, square_size=(0.25, 0.25))
+    manager = launch_stim_server(aux_screen)
+    niusb_device = None
 
 manager.black_corner_square()
 manager.set_idle_background(0.5)
@@ -36,7 +37,7 @@ offsets = []
 rotation_0 = {'name': 'RotatingGrating', 'angle': 0, 'period': period, 'rate': rate, 'color': 1, 'hold_duration': .5, 'phi': -45, 'offset': 180}
 rotation_180 = {'name': 'RotatingGrating', 'angle': 180, 'period': period, 'rate': rate, 'color': 1, 'hold_duration': .5,'phi': -45, 'offset': 0}
 long_grey = {'name': 'ConstantBackground'}
-long_grey_dur = 1#60
+long_grey_dur = 60
 
 #################
 ### SET THESE ###
@@ -55,21 +56,31 @@ epoch_cluster_duration *= 60 # now in sec
 epoch_duration = post_time + stim_time
 # given the duration of a single epoch and a cluster of epochs, how many epochs to present?
 num_epochs_in_cluster = int(epoch_cluster_duration / epoch_duration)
-num_single_stim_type = int(num_epochs_in_cluster/2) # just divide in 2 because we have left and right movement
+num_single_stim_type = int(num_epochs_in_cluster/3) # divide in 3 because we have left and right movement, and translation
 
 epoch_cluster_list = []
 for i in range(num_single_stim_type):
-	epoch_cluster_list.append(rotation_0.copy())
-	epoch_cluster_list.append(rotation_180.copy())
+    ### adding translation key. will remove below before passing to flystim
+    stim = rotation_0.copy()
+    stim['Translation']=False
+    epoch_cluster_list.append(stim)
+
+    stim = rotation_180.copy()
+    stim['Translation']=False
+    epoch_cluster_list.append(stim)
+
+    stim = rotation_0.copy()
+    stim['Translation']=True
+    epoch_cluster_list.append(stim)
 
 random.shuffle(epoch_cluster_list)
 
 ### Shuffle phase to avoid spatial clustering in optic lobes
 phase_offsets = list(np.arange(0,360,20)) # sample every 20 degrees
 for i in range(len(epoch_cluster_list)):
-	if epoch_cluster_list[i]['name'] == 'RotatingGrating':
-		random_offset = int(sample(phase_offsets,1)[0]) # add random offset
-		epoch_cluster_list[i]['offset'] += random_offset
+    if epoch_cluster_list[i]['name'] == 'RotatingGrating':
+        random_offset = int(sample(phase_offsets,1)[0]) # add random offset
+        epoch_cluster_list[i]['offset'] += random_offset
 
 ##################
 ### Save order ###
@@ -78,16 +89,19 @@ for i in range(len(epoch_cluster_list)):
 ### extract metadata
 angles = []
 offsets = []
+translation = []
 for stim in epoch_cluster_list:
-	angles.append(stim['angle'])
-	offsets.append(stim['offset'])
+    angles.append(stim['angle'])
+    offsets.append(stim['offset'])
+    translation.append(stim['Translation'])
 
 ### save to hdf5
 save_file = os.path.join('.', time.strftime("%Y%m%d-%H%M%S") + '.hdf5')
 with h5py.File(save_file,'w') as h5:
-	h5.create_dataset("data", data=np.asarray(angles)) # keeping "data" for backwards compatability
-	h5.create_dataset("angle", data=np.asarray(angles))
-	h5.create_dataset("offset", data=np.asarray(offsets))
+    h5.create_dataset("data", data=np.asarray(angles)) # keeping "data" for backwards compatability
+    h5.create_dataset("angle", data=np.asarray(angles))
+    h5.create_dataset("offset", data=np.asarray(offsets))
+    h5.create_dataset("translation", data=np.asarray(translation))
 
 #######################################################
 ### CONCATENATE EPOCH CLUSTERS BETWEEN GREY PERIODS ###
@@ -103,20 +117,39 @@ epoch_cluster_list.append(long_grey)
 
 # Send triggering TTL through the NI-USB device (if device is set)
 if niusb_device is not None:
-	niusb_device.sendTrigger()
+    niusb_device.sendTrigger()
 
 #######################
 ### DISPLAY STIMULI ###
 #######################
 
 for stim in epoch_cluster_list:
-	manager.load_stim(**stim)
-	manager.start_corner_square()
-	manager.start_stim()
-	if stim['name'] == 'ConstantBackground':
-		sleep(long_grey_dur)
-	else:
-		sleep(stim_time)
-	manager.stop_stim()
-	manager.black_corner_square()
-	sleep(post_time)
+
+
+    # pop will remove this key from the dict as well
+    # this is good because flystim will not recognize this key
+    translation = stim.pop('Translation', None)
+    manager.load_stim(**stim, hold=True)
+
+    if translation:
+        stim['angle'] -= 180
+        #stim['offset'] = 180
+        #stim['color'] = [1, 0, 1, 1]
+        stim['cylinder_location'] = (-.001,0,0)
+        manager.load_stim(**stim, hold=True)
+
+    multicall = flyrpc.multicall.MyMultiCall(manager)
+    multicall.start_corner_square()
+    multicall.start_stim()
+    multicall()
+
+    if stim['name'] == 'ConstantBackground':
+        sleep(long_grey_dur)
+    else:
+        sleep(stim_time)
+
+    multicall = flyrpc.multicall.MyMultiCall(manager)
+    multicall.stop_stim()
+    multicall.black_corner_square()
+    multicall()
+    sleep(post_time)
